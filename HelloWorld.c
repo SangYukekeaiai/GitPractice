4700ee2c8096a8f9ee565696ec53035e38176677
YY <YYu11131719@outlook.com>
2022-02-18 08:58:32 +0800

    New file added

---
+++ HelloWorld.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,sum;
+    printf("hello world!\n");
+    scanf("%d %d", &a, &b);
+    sum = a + b;
+    printf("%d+%d = %d\n", a, b, sum);
+    printf("YY is pretty! What a lovely day!\n");
+    return 0;
+}
